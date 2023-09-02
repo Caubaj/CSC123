a958841aeafb0e19999385d35a8aa90cc4a142de
carlo <carlo@10.163.34.165>
2023-09-02 15:31:32 -0700

    yo

---
+++ Week1/src/CSC/first.java
@@ -0,0 +1,19 @@
+package CSC;
+
+import java.util.Scanner;
+
+public class first {
+
+	public static void main(String[] args) 
+	{
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
+		System.out.println("Hello world!");
+		
+		
+	}
+
+}
+	
