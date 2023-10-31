666b08c74b5e37dcadb75f1c30f8c37ba101df7e
carlo <carlo@TABLET-N6L0G670.lan>
2023-10-30 22:44:42 -0700

    Assignment 3

--- Week1/Assigment3/Item.java
+++ Week1/Assigment3/Item.java
@@ -1,4 +1,86 @@
+import java.text.DecimalFormat;
 
 public class Item {
+	
+	//cportillomedina1@toromail.csudh.edu
+	
+		public String itemName;
+		public String vendor;
+		public double price;
+		public double cost;
+		public double weight;
+		public double taxRate;
+		public double itemTax;
+		
+		public String author;
+		public String pMonth;
+		public int pages;
+		
+		public String sellBy;
+		public String useBy;
+		
+		public DecimalFormat decfor = new DecimalFormat("0.00");
+		
+		
+		// for publication items
+		public Item(String name, double weigh, 
+				String aut, String pMon, int pages, double price, double tax)
+		{
+			itemName = name;
+			weight = weigh;
+			author  = aut;
+			pMonth = pMon;
+			this.pages = pages;
+			this.price = price;
+			taxRate = tax;
+		}
+		
+		// for food items
+		public Item(String name, double wei, 
+				double pr, double tax, String sell, String use)
+		{
+			itemName = name;
+			weight = wei;
+			price = pr;
+			taxRate = tax;
+			sellBy = sell;
+			useBy = use;
+		}
+		
+		//for genral grocery items
+		public Item(String name, double wei, 
+				double pr, double tax)
+		{
+			itemName = name;
+			weight = wei;
+			price = pr;
+			taxRate = tax;
+		}
+		
+		public double setTax()
+		{
+			
+			double tax = price * (taxRate / 100); 
+			return tax;
+		}
+		
+		public String setTax2()
+		{
+			return decfor.format(price * (taxRate / 100));
+		}
+		
+		public String setTotal()
+		{	
+			return decfor.format(setTax() + price);
+		}
+		
+		public Double setTotal2()
+		{	
+			return setTax() + price;
+		}
+		public String toString()
+		{
+			return "Item: " + itemName + " | Price: $" + price + " | Item Tax: $" + setTax2() + " | Item Total: $" + setTotal();
+		}
 
 }
--- Week1/Assigment3/ShoppingCart.java
+++ Week1/Assigment3/ShoppingCart.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ShoppingCart {
 
@@ -5,7 +7,159 @@ public class ShoppingCart {
 	
 	{
 		
+		//cportillomedina1@toromail.csudh.edu
 		
+				ArrayList <String> cart = new ArrayList<String>();	
+				int x = 0;
+				byte add;
+				byte type;
+				
+				String name;
+			//	String vendor = "N/A";
+				double price;
+			//	double cost;
+				double weight;
+				double taxRate;
+				double total = 0;
+				
+				String author;
+				String pMonth;
+				int pages;
+				
+				String sellBy;
+				String useBy;
+				
+				
+				Scanner scan = new Scanner(System.in);
+				
+				while(x != -1)
+				{
+					System.out.println("Would you like to add an item to your cart?\n Press 1 for Yes | Press 2 to exit | Press 3 to view cart");
+					add = scan.nextByte();
+						while(add != 1 && add != 2 && add != 3)
+						{
+							System.out.println("Error! Please try again!");
+							System.out.println("Would you like to add an item to your cart?\n Press 1 for Yes | Press 2 to exit | Press 3 to view cart");
+							add = scan.nextByte();
+						}
+					if(add == 1)
+					{
+						System.out.println("Is your item a publication, food, or general grocery item?\n Type 1 for publication | 2 for food | 3 for general grocery item" );
+						type = scan.nextByte();
+						scan.nextLine();
+						
+						if(type == 1)
+						{
+							System.out.println("What is the name of your item?");
+							name = scan.nextLine();
+							
+							System.out.println("What is the weight of your item? (in pounds)");
+							weight = scan.nextDouble();
+							
+							System.out.println("Who is the author of your item?");
+							author = scan.nextLine();
+							scan.nextLine();
+							
+							System.out.println("What is the publication month of your item?");
+							pMonth = scan.nextLine();
+							
+							System.out.println("How many pages does your item have?");
+							pages = scan.nextInt();
+							
+							System.out.println("What is the price of your item?");
+							price = scan.nextDouble(); 
+							
+							System.out.println("What is the tax rate of your item?");
+							taxRate = scan.nextDouble(); 
+							
+							Item item = new Item(name, weight, author, pMonth, pages, price, taxRate);
+							
+							cart.add(item.toString());
+							
+							total = total + item.setTotal2();	
+						}
+						else if (type == 2)
+						{
+						System.out.println("What is the name of your item?");
+						name = scan.nextLine();
+						
+						System.out.println("What is the weight of your item? (in pounds)");
+						weight = scan.nextDouble();
+						
+					//  System.out.println("Who is the vendor of your Item?");
+					//	vendor = scan.nextLine(); 
+						
+						System.out.println("What is the price of your item?");
+						price = scan.nextDouble(); 
+						
+					//	System.out.println("What is the cost of your item?");
+					//	cost = scan.nextDouble(); 
+						
+						System.out.println("What is the tax rate of your item?");
+						taxRate = scan.nextDouble(); 
+						
+						System.out.println("When does your item sell by?");
+						sellBy = scan.nextLine(); 
+						scan.nextLine();
+						
+						System.out.println("When should your item be used by?");
+						useBy = scan.nextLine(); 
+						
+						Item item = new Item(name, weight, price, taxRate, sellBy, useBy);
+						
+						cart.add(item.toString());
+						
+						total = total + item.setTotal2();
+						
+						
+						}
+						else if (type == 3)
+						{
+							
+							System.out.println("What is the name of your item?");
+							name = scan.nextLine();
+							
+							System.out.println("What is the weight of your item? (in pounds)");
+							weight = scan.nextDouble();
+							
+						//  System.out.println("Who is the vendor of your Item?");
+						//	vendor = scan.nextLine(); 
+							
+							System.out.println("What is the price of your item?");
+							price = scan.nextDouble(); 
+							
+						//	System.out.println("What is the cost of your item?");
+						//	cost = scan.nextDouble(); 
+							
+							System.out.println("What is the tax rate of your item?");
+							taxRate = scan.nextDouble(); 
+							
+							Item item = new Item(name, weight, price, taxRate);
+							
+							cart.add(item.toString());
+							
+							total = total + item.setTotal2();	
+						}	
+					}
+					if(add == 2)
+					{
+						System.out.println("Thank You for using cart! Have a fantastic Day!");
+						x = -1;	
+					}
+					if(add == 3)
+					{
+						System.out.println();
+						System.out.println("You have " + cart.size() + " item(s) in your cart.");
+						
+						for (int y = 0; y < cart.size(); y++)
+						{
+							System.out.println(cart.get(y));	             	
+						}
+						System.out.printf("\nCart total: $%.2f\n" , total);		
+					}
+					
+				}
+				
 		
 
 	}
